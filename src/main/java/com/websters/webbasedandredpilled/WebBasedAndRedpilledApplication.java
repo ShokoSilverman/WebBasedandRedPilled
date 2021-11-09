b879d7b91a00d3df1ebd70b215b1c958e726ec43
Simon Silverman <ssilverman@student.neumont.edu>
2021-11-09 09:13:12 -0700

    Initial commit

---
+++ src/main/java/com/websters/webbasedandredpilled/WebBasedAndRedpilledApplication.java
@@ -0,0 +1,13 @@
+package com.websters.webbasedandredpilled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedAndRedpilledApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBasedAndRedpilledApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/websters/webbasedandredpilled/WebBasedAndRedpilledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websters.webbasedandredpilled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBasedAndRedpilledApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
