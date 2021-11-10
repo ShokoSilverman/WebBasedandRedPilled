93612333e4f499d245ee90b38a66d3e14854fae1
Simon Silverman <ssilverman@student.neumont.edu>
2021-11-10 07:15:41 -0700

    added express, mongo, http, and socket.io node modules, also set up some more of the skeleton classes

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,3 +1,8 @@
+//const express = require('express'); had to do these to get them installed, there is not a simple npm install window -_-
+//const {MongoClient, ObjectId} = require("mongodb");
+// const http = require('http');
+// const { Server } = require("socket.io");
+
 let testOutVal = ''
 
 testInput = () => {
