bbc44028bb68508a07116e8dde81df89d984f964
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2021-11-12 14:43:02 -0700

    Create MainControllerBLL.java

---
+++ src/main/java/com/websters/webbasedandredpilled/MainControllerBLL.java
@@ -0,0 +1,13 @@
+package com.websters.webbasedandredpilled;
+
+import java.util.ArrayList;
+
+public class MainControllerBLL {
+    //List of all Messages sent by User between Login and Sign out
+    public ArrayList<MessagePOJO> messageList = new ArrayList<>();
+    //List of all Errors that occur while User is on any page
+    public ArrayList<ErrorLog> errorList = new ArrayList<>();
+
+   
+
+}
