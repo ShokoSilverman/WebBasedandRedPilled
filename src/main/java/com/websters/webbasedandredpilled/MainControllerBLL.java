3a9c258055425bd2c27e41d3749e31ef7716ceee
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2021-11-15 14:50:38 -0700

    Update MainControllerBLL.java

--- src/main/java/com/websters/webbasedandredpilled/MainControllerBLL.java
+++ src/main/java/com/websters/webbasedandredpilled/MainControllerBLL.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class MainControllerBLL {
     //List of all Messages sent by User between Login and Sign out
+    //TODO Make this a Session Variable
     public ArrayList<MessagePOJO> messageList = new ArrayList<>();
     //List of all Errors that occur while User is on any page
     public ArrayList<ErrorLog> errorList = new ArrayList<>();
