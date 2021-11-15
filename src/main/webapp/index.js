97ee997682df481d2e7826dc78431b3612a7f5a6
culintim000 <cultim000@gmail.com>
2021-11-15 13:44:18 -0700

    working multiple users sharing chats

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/chatRoom.html');
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+io.on('connection', (socket) => {
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
