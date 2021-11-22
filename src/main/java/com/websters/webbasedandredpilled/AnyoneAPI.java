e87724d5bf006e17bd47814c636b3661910f6f1e
culintim000 <cultim000@gmail.com>
2021-11-22 14:49:26 -0700

    progress on exception handling

--- src/main/java/com/websters/webbasedandredpilled/AnyoneAPI.java
+++ src/main/java/com/websters/webbasedandredpilled/AnyoneAPI.java
@@ -1,10 +1,7 @@
 package com.websters.webbasedandredpilled;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/anyone")
@@ -12,5 +9,32 @@ public class AnyoneAPI {
     @RequestMapping(path = "/index", method= RequestMethod.GET)
     public String speak() {
         return "this reached anyone";
+    }
+
+    @GetMapping(path = "/{action}") // Different than before
+    public String findById(@PathVariable String action) {
+
+        if (action != null)    {
+            switch(action) {
+                case "0":
+                    System.out.println("Enter action is 0");
+                    return "Hello World";
+                case "1":
+                    System.out.println("Enter action = 1: IllegalArgumentException");
+                    throw  new IllegalArgumentException("My Error IllegalArgumentException. Action is 1");
+                case "2":
+                    System.out.println("Enter action = 2: RuntimeException");
+                    throw new RuntimeException("My error RuntimeException. Action is 2");
+                case "3":
+                    System.out.println("Enter action = 3: IllegalCallerException");
+                    throw new IllegalCallerException("My error Illegal Caller Exception. Action is 3");
+                default:
+                    System.out.println("Enter no action");
+                    return "action value not in the range of 0 through 3";
+            }
+
+        }
+        else
+            return "Please include action=0-3 (example /1)";
     }
 }
--- src/main/java/com/websters/webbasedandredpilled/ErrorHandler.java
+++ src/main/java/com/websters/webbasedandredpilled/ErrorHandler.java
@@ -1,4 +1,38 @@
 package com.websters.webbasedandredpilled;
 
-public class ErrorHandler {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ErrorHandler implements ErrorController {
+    @RequestMapping(path="/error", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ExceptionMessage handleError(HttpServletRequest req, HttpServletResponse res) {
+
+        Integer statusCode = (Integer) req.getAttribute("javax.servlet.error.status_code");
+        System.out.println("******status code: " + statusCode);
+
+        Exception exception = (Exception) req.getAttribute("javax.servlet.error.exception");
+        System.out.println("*****exception: " + exception);
+
+        ExceptionMessage message = new ExceptionMessage("My exception message: " + exception.getMessage()
+                ,statusCode, exception);
+        System.out.println("****the message: " + message);
+
+
+//        res.setStatus(HttpServletResponse.SC_FORBIDDEN); //403
+        return message;
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
 }
+
