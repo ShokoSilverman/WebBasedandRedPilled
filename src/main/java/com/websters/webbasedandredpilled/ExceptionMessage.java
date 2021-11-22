2a53f48ca035fea612428cbd95c97e8779ea83ad
culintim000 <cultim000@gmail.com>
2021-11-22 14:26:42 -0700

    added skeleton for error exceptions and anyone API

--- src/main/java/com/websters/webbasedandredpilled/ExceptionMessage.java
+++ src/main/java/com/websters/webbasedandredpilled/ExceptionMessage.java
@@ -1,4 +1,45 @@
 package com.websters.webbasedandredpilled;
 
 public class ExceptionMessage {
+    private String exceptionMessage;
+    private Integer exceptionHttpStatus;
+    private Throwable exceptionThrowable = null;
+
+    public ExceptionMessage(String message, Integer httpStatus, Throwable th) {
+        this.exceptionThrowable = th;
+        this.exceptionMessage = message;
+        this.exceptionHttpStatus = httpStatus;
+    }
+
+    public ExceptionMessage(String message, Integer httpStatus) {
+        this.exceptionMessage = message;
+        this.exceptionHttpStatus = httpStatus;
+    }
+
+    public String getExceptionMessage() {
+        return exceptionMessage;
+    }
+
+    public void setExceptionMessage(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    public Integer getExceptionHttpStatus() {
+        return exceptionHttpStatus;
+    }
+
+    public void setExceptionHttpStatus(Integer exceptionHttpStatus) {
+        this.exceptionHttpStatus = exceptionHttpStatus;
+    }
+
+    public Throwable getExceptionThrowable() {
+        return exceptionThrowable;
+    }
+
+    public void setExceptionThrowable(Throwable exceptionThrowable) {
+        this.exceptionThrowable = exceptionThrowable;
+    }
+
+
 }
+
